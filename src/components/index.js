accb848362b6f9555b4bd5424b8334137a3e6d9f
zhouLinyang <1034637588@qq.com>
2020-12-27 15:56:54 +0800

    添加单滑块组件

--- src/components/index.js
+++ src/components/index.js
@@ -1,10 +1,13 @@
 import DoubleSlider from './DoubleSlider'
+import SingleSlider from './SingleSlider'
 const install = function (Vue) {
     // 将组件进行挂载
     Vue.component('DoubleSlider',DoubleSlider)
+    Vue.component('SingleSlider',singleSlider)
 }
 if(typeof window !== 'undefined' && window.Vue){
     window.Vue.use(DoubleSlider);
+    window.Vue.use(SingleSlider);
 }
 export default {
     install
